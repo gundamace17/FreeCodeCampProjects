d9410346b9c4e1297312d7e8a026bd54650e89b7
RileyL <51372465+gundamace17@users.noreply.github.com>
2020-09-19 21:24:21 -0700

    Update arithmetic arranger.py

--- Scientific Computing with Python Projects/arithmetic arranger.py
+++ Scientific Computing with Python Projects/arithmetic arranger.py
@@ -57,6 +57,3 @@ def arithmetic_arranger(problems, bol = False):
     print(arranged_problems)
     return arranged_problems
     
-
-    
-arithmetic_arranger(["32 - 698", "1 - 3801", "45 + 43", "123 + 49"], True)
